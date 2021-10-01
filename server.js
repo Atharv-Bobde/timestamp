e40416c3fd592ea3d66c7e64fb1d7094942359d3
atharvbobde123@gmail.com <atharvbobde123@gmail.com>
2021-10-01 22:50:43 +0530

    A

--- server.js
+++ server.js
@@ -4,23 +4,28 @@ require('dotenv').config()
 // init project
 var express = require('express');
 var app = express();
+const path=require('path')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const ServerlessHttp = require('serverless-http');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+//router
+const router=express.Router();
+
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname ,'../views/index.html'));
 });
 
 
 // your first API endpoint... 
-app.get("/api/:date", function (req, res) {
+router.get("/api/:date", function (req, res) {
   let l=new Date(req.params.date);
   let regx=/\D/;
   if(!regx.test(req.params.date))
@@ -30,12 +35,13 @@ app.get("/api/:date", function (req, res) {
   else
     res.json({'unix':Date.parse(l),'utc':l.toUTCString()});
 });
-app.get("/api", function (req, res) {
+router.get("/api", function (req, res) {
   const curr_time=new Date().toUTCString();
   res.json({'unix':Date.parse(curr_time),'utc':curr_time});
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+
+
+app.use('/.netlify/functions/server',router);
+
+module.exports.handler=ServerlessHttp(app);
